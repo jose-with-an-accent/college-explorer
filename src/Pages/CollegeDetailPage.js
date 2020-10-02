3b76b6f0d30bd07f86e8cef857fe26283e2dc9ce
Jose Sanchez <josersanchez0117@gmail.com>
2020-10-01 21:25:17 -0500

    WIP. See next commit.

--- src/Pages/CollegeDetailPage.js
+++ src/Pages/CollegeDetailPage.js
@@ -9,29 +9,30 @@ export default function CollegeDetailPage() {
     return (
         <FirebaseAuthConsumer>
             {({ isSignedIn, user }) => {
-                console.log(user)
                 return (
                     user && <FirebaseDatabaseNode path={`user_data/${user.uid}`}>
                         {userDatabase => {
-                            return <FirebaseDatabaseNode path={`all_colleges/${id}`}>
-                                {collegeInfo => {
+                            const collegeChoices = userDatabase.value
+                            if (userDatabase.value) { return <FirebaseDatabaseNode path={`all_colleges/${id}`}>
+                                {value => {
+                                    const collegeInfo = value.value
                                     console.log(collegeInfo);
-                                    if (collegeInfo.value != null) {
+                                    if (collegeInfo != null) {
                                         return (
                                             <div className="collegeDetailPage">
-                                                <div id="headerImageBg" style={{ backgroundImage: `url: ${collegeInfo.value.bgImage}` }}></div>
+                                                <div id="headerImageBg" style={{ backgroundImage: `url: ${collegeInfo.bgImage}` }}></div>
                                                 <header className="h">
-                                                    <h1>{collegeInfo.value.name}</h1>
+                                                    <h1>{collegeInfo.name}</h1>
                                                 </header>
                                                 <div className="flex flex-row">
                                                     <div className="collegeDetailMain">
                                                         <InfoSection title="Overview">
-                                                            {collegeInfo.value.description}
+                                                            {collegeInfo.description}
                                                         </InfoSection>
                                                         <InfoSection title="Admissions">
-                                                            {collegeInfo.value.admissionsAverages != null ? <ul>
-                                                                <li>The middle 50% SAT Range is {collegeInfo.value.admissionsAverages.sat.minAvgRange} - {collegeInfo.value.admissionsAverages.sat.maxAvgRange} </li>
-                                                                <li>The middle 50% ACT Range is {collegeInfo.value.admissionsAverages.act.minAvgRange} - {collegeInfo.value.admissionsAverages.act.maxAvgRange} </li>
+                                                            {collegeInfo.admissionsAverages != null ? <ul>
+                                                                <li>The middle 50% SAT Range is {collegeInfo.admissionsAverages.sat.minAvgRange} - {collegeInfo.admissionsAverages.sat.maxAvgRange} </li>
+                                                                <li>The middle 50% ACT Range is {collegeInfo.admissionsAverages.act.minAvgRange} - {collegeInfo.admissionsAverages.act.maxAvgRange} </li>
                                                             </ul> : <h2>No Admissions Averages found :(</h2>}
                                                         </InfoSection>
                                                         <InfoSection title="Programs Offered">
@@ -48,25 +49,24 @@ export default function CollegeDetailPage() {
                                                             <span className="bold">Application Deadlines:</span>
                                                             <ul>
 
-                                                                {collegeInfo.value.deadlines ? collegeInfo.value.deadlines.map((val, ind) => {
+                                                                {collegeInfo.deadlines ? collegeInfo.deadlines.map((val, ind) => {
                                                                     console.log(val.cappDueBy)
                                                                     return <li className="">{val.name}: {new Date(val.appDueBy).toDateString()}</li>
                                                                 }) : <li>No App Deadlines found.</li>}
                                                             </ul>
                                                         </div>
                                                         {
-                                                        {
-                                                            collegeChoices = userDatabase.value.user_data[user.uid].college_choices
-                                                        userDatabase.value && collegeChoices.includes(collegeInfo.id) ? <Button text="Added To List" width="fw" /> : <FirebaseDatabaseMutation type="push" path={`/user_data/${user.uid}/college_choices`}>
-                                                            {({runMutation}) => {
-                                                            return <Button text="Add To List" width="fw" onClick={
-                                                                async() => {
-                                                                    const a = await runMutation({})
-                                                                    console.log(a)
-                                                                }
-                                                            }>Add To List</Button>
-                                                            }}
-                                                            </FirebaseDatabaseMutation>}}
+                                                            userDatabase.value && collegeChoices.includes(collegeInfo.id) ? <Button text="Added To List" width="fw" /> : <FirebaseDatabaseMutation type="push" path={`/user_data/${user.uid}/college_choices`}>
+                                                                {({ runMutation }) => {
+                                                                    return <Button text="Add To List" width="fw" onClick={
+                                                                        async () => {
+                                                                            const a = await runMutation({})
+                                                                            console.log(a)
+                                                                        }
+                                                                    }>Add To List</Button>
+                                                                }}
+                                                            </FirebaseDatabaseMutation>
+                                                        }
                                                         <Button text="Working on it!" width="fw" type="secondary" />
                                                     </aside>
                                                 </div>
@@ -75,10 +75,13 @@ export default function CollegeDetailPage() {
                                     } else {
                                         return null
                                     }
+                                    
                                 }
                                 }
                             </FirebaseDatabaseNode>
-                        }}
+                        } else return null
+                    }
+                    }
                     </FirebaseDatabaseNode>)
             }}
         </FirebaseAuthConsumer>
